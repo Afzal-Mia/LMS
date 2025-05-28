ed581f3a416ab01e1a0f9f9cb0ef27adbf1a8660
Afzal Ansari <ansarahemad1220@gmail.com>
2025-05-28 23:24:06 +0530

    added backend hostedl uri

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:1200";
+export const server = "https://lms-backend-ls35.onrender.com/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <UserContextProvider>
