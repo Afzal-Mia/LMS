90432f6cd35152d2240b83ab841e0c1d64729a79
Afzal Ansari <ansarahemad1220@gmail.com>
2025-05-28 23:48:25 +0530

    url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
 export const server = "https://lms-backend-ls35.onrender.com/";
+console.log("server");
+console.log(server);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <UserContextProvider>
